b9aa4c06d48e7910d3483213af7bb88beaabab36
Jordan Curzon <curzonj@gmail.com>
2018-09-20 20:03:23 -0700

    Disable the service worker because it causes problems with graphql and isn't worth it yet

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/App';
-import registerServiceWorker from './utils/registerServiceWorker';
+import { unregister } from './utils/registerServiceWorker';
 import { handleOAuthCallback } from './utils/authentication';
 
 handleOAuthCallback().then(() => {
   ReactDOM.render(<App />, document.getElementById('root'));
 });
 
-registerServiceWorker();
+unregister();
