d98f0b13afd5a05ce7f0425b852a0c7cd82b2dfc
Jordan Curzon <curzonj@gmail.com>
2018-09-23 10:47:11 -0700

    Change build process to reduce slug size

--- bin/build.sh
+++ bin/build.sh
@@ -3,5 +3,17 @@
 set -x
 set -e
 
+pushd api
 npm install --production=false
 npm run build
+popd
+
+pushd client
+npm install --production=false
+npm run build
+popd
+
+mv client/build ./
+rm -rf client
+mkdir -p client
+mv build client/
